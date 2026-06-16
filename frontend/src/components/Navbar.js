2fc861170a8df760979496c267fa037841a0b667
anshsoni77 <anshsoni141@gmail.com>
2026-06-17 00:03:14 +0530

    Fix final eslint warning

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 const Navbar = () => {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
-  const [menuOpen, setMenuOpen] = useState(false);
+  //const [menuOpen, setMenuOpen] = useState(false);
 
   const handleLogout = () => {
     logout();
